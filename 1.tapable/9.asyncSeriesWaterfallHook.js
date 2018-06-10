19854612d3119d0697a6f44028a599089f538bd1
zhangxk <zhangxkmail@163.com>
2018-06-10 09:46:04 +0800

    modify

--- 1.tapable/9.asyncSeriesWaterfallHook.js
+++ 1.tapable/9.asyncSeriesWaterfallHook.js
@@ -21,4 +21,56 @@ hook.callAsync('webpack', data => {
 */
 
 
+const queue2 =new AsyncSeriesWaterfallHook();
+
+console.time('cost2');
+queue2.tapAsync('1', function (name, callback) {
+    setTimeout(function () {
+        console.log('1: ', name);
+        callback(null, 2);
+    }, 100)
+});
+queue2.tapAsync('2', function (data, callback) {
+    setTimeout(function () {
+        console.log('2: ', data);
+        callback(null, 3);
+    }, 200)
+});
+queue2.tapAsync('3', function (data, callback) {
+    setTimeout(function () {
+        console.log('3: ', data);
+        callback(null, 3);
+    }, 300)
+});
+queue2.callAsync('webpack', err => {
+    console.log(err);
+    console.log('over');
+    console.timeEnd('cost2');
+});
+
+// hook1.tapAsync('plugin1', (cb) => {
+//   setTimeout(() => {
+//     cb(1)
+//     console.log('plugin1');
+//   }, 100)
+// })
+
+// hook1.tapAsync('plugin2', (cb) => {
+//   setTimeout(() => {
+//     cb(2);
+//     console.log('plugin2');
+//   }, 200)
+// })
+
+// hook1.tapAsync('plugin3', (cb) => {
+//   setTimeout(() => {
+//     cb(3)
+//     console.log('plugin3');
+//   }, 300)
+// })
+
+// hook1.callAsync( (data) => {
+//   console.log('callAsync 触发', data)
+// })
+
 
