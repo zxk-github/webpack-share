8b8266ad47a4360128234da8cd52c072c2b7d965
zhangxk <zhangxkmail@163.com>
2018-06-11 11:04:45 +0800

    add

--- 1.tapable/9.asyncSeriesWaterfallHook.js
+++ 1.tapable/9.asyncSeriesWaterfallHook.js
@@ -12,65 +12,90 @@ hook.tap('plugin2', (data) => {
   return 2;
 })
 
-hook.callAsync('webpack', data => {
-  console.log(data)
-});
+// hook.callAsync('webpack', data => {
+//   console.log(data)
+// });
+hook.promise().then((data) => {
+  console.log('promise 触发', data)
+})
 
+// 使用AsyncSeriesWaterfallHook 在初始化时候，必须传入参数
 // 上一个插件的返回结果，会做为下一个插件的参数使用
+*/
+
+/*
+
+const hook2 = new AsyncSeriesWaterfallHook(['name']);
+
+hook2.tapAsync('plugin1', (data, cb) => {
+  setTimeout(() => {
+    console.log('plugin1', data);
+    cb(null, 1);
+  }, 200)
+})
+
+
+hook2.tapAsync('plugin2', (data, cb) => {
+  setTimeout(() => {
+    console.log('plugin2', data);
+    cb(null, 2);
+  }, 300)
+})
+
+hook2.tapAsync('plugin3', (data, cb) => {
+  setTimeout(() => {
+    console.log('plugin3', data);
+    cb(null, 3);
+  }, 300)
+})
+
+// hook2.callAsync('hook2',(data) => {
+//   console.log('callAsync触发', data)
+// })  
+
+hook2.promise().then((data) => {
+  console.log("promise触发", data)
+})
+
+//  使用promise触发，可以拿到最后一个插件，callback的值, callAsync则拿不到
 
 */
 
+const hook3 = new AsyncSeriesWaterfallHook(['name']);
 
-const queue2 =new AsyncSeriesWaterfallHook();
-
-console.time('cost2');
-queue2.tapAsync('1', function (name, callback) {
-    setTimeout(function () {
-        console.log('1: ', name);
-        callback(null, 2);
-    }, 100)
-});
-queue2.tapAsync('2', function (data, callback) {
-    setTimeout(function () {
-        console.log('2: ', data);
-        callback(null, 3);
+hook3.tapPromise('plugin1', (data) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('plugin1', data)
+      resolve(1)
     }, 200)
-});
-queue2.tapAsync('3', function (data, callback) {
-    setTimeout(function () {
-        console.log('3: ', data);
-        callback(null, 3);
+  })
+})
+
+hook3.tapPromise('plugin2', (data) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('plugin2', data)
+      resolve(2)
     }, 300)
-});
-queue2.callAsync('webpack', err => {
-    console.log(err);
-    console.log('over');
-    console.timeEnd('cost2');
-});
+  })
+})
 
-// hook1.tapAsync('plugin1', (cb) => {
-//   setTimeout(() => {
-//     cb(1)
-//     console.log('plugin1');
-//   }, 100)
+hook3.tapPromise('plugin3', (data) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('plugin3', data)
+      resolve(3)
+    }, 300)
+  })
+})
+
+// hook3.promise('hook3').then((data) => {
+//   console.log('promise', data)
+// }, (err) => {
+//   console.log(err)
 // })
 
-// hook1.tapAsync('plugin2', (cb) => {
-//   setTimeout(() => {
-//     cb(2);
-//     console.log('plugin2');
-//   }, 200)
-// })
-
-// hook1.tapAsync('plugin3', (cb) => {
-//   setTimeout(() => {
-//     cb(3)
-//     console.log('plugin3');
-//   }, 300)
-// })
-
-// hook1.callAsync( (data) => {
-//   console.log('callAsync 触发', data)
-// })
-
-
+hook3.callAsync('hook3', (data) => {
+  console.log('callAsync触发', data)
+})
