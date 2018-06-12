ebc12ffd2e0cf21bb6bb3eeca8abf470e69b122a
zhangxk <zhangxkmail@163.com>
2018-06-12 12:17:44 +0800

    modify

---
+++ 2.webpack4.0/webpack.config.js
@@ -0,0 +1,21 @@
+let path = require('path');
+
+module.exports = {
+  mode: 'development',
+
+  entry : './entry.js',
+
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, './dist')
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+}
